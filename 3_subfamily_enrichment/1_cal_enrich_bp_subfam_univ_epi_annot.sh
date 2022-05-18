3b05a5b589822ca4cece7336addde0f302629498
Ju Heon Maeng <jmaeng@woodchuck.wustl.edu>
2022-05-18 13:54:28 -0500

    wscripts and data for fig3-5

---
+++ 3_subfamily_enrichment/1_cal_enrich_bp_subfam_univ_epi_annot.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# Author: Hyung Joo Lee
+
+
+# SOFTWARE
+ml bedtools/2.27.1
+
+# GENOME FEATURES
+subfam_bed=$(ls ../annotation/TEsubfamily/*bed.gz)
+genome_size=../annotation/danRer10.chrom.sizes
+
+
+# INPUT DATA
+bed_prom=../0_CRE_file_prepared/universal/uni_active_promoter.bed.gz
+bed_weak=../0_CRE_file_prepared/universal/uni_weak_promoter.bed.gz
+bed_enhc=../0_CRE_file_prepared/universal/uni_active_enhancer.bed.gz
+
+bed_prox=../0_CRE_file_prepared/universal/uni_proximal_ATAC.bed.gz
+bed_dist=../0_CRE_file_prepared/universal/uni_distal_ATAC.bed.gz
+
+bed_umr=../0_CRE_file_prepared/universal/uni_UMR.bed.gz
+bed_lmr=../0_CRE_file_prepared/universal/uni_LMR.bed.gz
+
+
+
+# OUTPUT
+dir_out=`pwd`
+out_prom=${dir_out}/subfam_enriched_active_promoter_univ.txt
+out_weak=${dir_out}/subfam_enriched_weak_promoter_univ.txt
+out_enhc=${dir_out}/subfam_enriched_enhancer_univ.txt
+
+out_prox=${dir_out}/subfam_enriched_proximal_ATAC_univ.txt
+out_dist=${dir_out}/subfam_enriched_distal_ATAC_univ.txt
+
+out_umr=${dir_out}/subfam_enriched_UMR_univ.txt
+out_lmr=${dir_out}/subfam_enriched_LMR_univ.txt
+
+
+# COMMANDS
+len_genome=$( awk 'BEGIN{s=0} {s+=$2} END{print s}' $genome_size )
+
+len_prom=$( mergeBed -i $bed_prom | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+len_weak=$( mergeBed -i $bed_weak | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+len_enhc=$( mergeBed -i $bed_enhc | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+
+len_prox=$( mergeBed -i $bed_prox | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+len_dist=$( mergeBed -i $bed_dist | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+
+len_umr=$( mergeBed -i $bed_umr | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+len_lmr=$( mergeBed -i $bed_lmr | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+
+
+for bed_te in $subfam_bed
+do
+    name_te=${bed_te##*/}
+    name_te=${name_te%.bed.gz}
+    len_te=$( mergeBed -i $bed_te | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+
+    bp_prom_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_prom | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_prom_te -v nd=$len_te -v dn=$len_prom -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_prom_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_prom | wc -l )
+    if [ $cnt_prom_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tActive_promoter\t$bp_prom_te\t$len_te\t$len_prom" >> $out_prom
+
+    bp_weak_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_weak | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_weak_te -v nd=$len_te -v dn=$len_weak -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_weak_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_weak | wc -l )
+    if [ $cnt_weak_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tWeak_promoter\t$bp_weak_te\t$len_te\t$len_weak" >> $out_weak
+
+    bp_enhc_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_enhc | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_enhc_te -v nd=$len_te -v dn=$len_enhc -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_enhc_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_enhc | wc -l )
+    if [ $cnt_enhc_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tActive_enhancer\t$bp_enhc_te\t$len_te\t$len_enhc" >> $out_enhc
+
+
+    bp_prox_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_prox | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_prox_te -v nd=$len_te -v dn=$len_prox -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_prox_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_prox | wc -l )
+    if [ $cnt_prox_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tProximal_ATAC\t$bp_prox_te\t$len_te\t$len_prox" >> $out_prox
+
+    bp_dist_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_dist | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_dist_te -v nd=$len_te -v dn=$len_dist -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_dist_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_dist | wc -l )
+    if [ $cnt_dist_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tDistal_ATAC\t$bp_dist_te\t$len_te\t$len_dist" >> $out_dist
+
+
+    bp_umr_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_umr | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_umr_te -v nd=$len_te -v dn=$len_umr -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_umr_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_umr | wc -l )
+    if [ $cnt_umr_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tUMR\t$bp_umr_te\t$len_te\t$len_umr" >> $out_umr
+
+
+    bp_lmr_te=$( intersectBed -sorted -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_lmr | sort -k1,1 -k2,2n | mergeBed | awk 'BEGIN{s=0} {s+=$3-$2} END{print s}' )
+    lor=$( awk -v nn=$bp_lmr_te -v nd=$len_te -v dn=$len_lmr -v dd=$len_genome 'BEGIN{ if (nn>0) { printf ("%.4f", log( (nn/nd)/(dn/dd) ) / log(2) ) } else {printf("NA")} }' )
+    cnt_lmr_te=$( intersectBed -sorted -wa -u -a <(zcat $bed_te | sort -k1,1 -k2,2n ) -b $bed_lmr | wc -l )
+    if [ $cnt_lmr_te -le 10 ]; then
+        lor="NA"; fi
+    echo -e "$lor\t$name_te\tUniversal\tLMR\t$bp_lmr_te\t$len_te\t$len_lmr" >> $out_lmr
+
+done
+
---
+++ 3_subfamily_enrichment/2_combine_ts_univ.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Author: Hyung Joo Lee
+
+#SBATCH --mem=2G
+##SBATCH --array=1-11
+##SBATCH --workdir=/scratch/twlab/hlee/zf_te
+#SBATCH --job-name=lor_subfam
+
+# SOFTWARE
+ml bedtools/2.27.1
+
+
+# GENOME FEATURES
+subfam_bed=$(ls ../annotation/TEsubfamily/*bed.gz)
+genome_size=../annotation/danRer10.chrom.sizes
+
+
+
+# INPUT DATA
+dir_in=`pwd`
+data_prom=${dir_in}/subfam_enriched_active_promoter_[bchiklmstu]*.txt
+data_weak=${dir_in}/subfam_enriched_weak_promoter_[bchiklmstu]*.txt
+data_enhc=${dir_in}/subfam_enriched_enhancer_[bchiklmstu]*.txt
+
+data_prox=${dir_in}/subfam_enriched_proximal_ATAC_[bchiklmstu]*.txt
+data_dist=${dir_in}/subfam_enriched_distal_ATAC_[bchiklmstu]*.txt
+
+data_umr=${dir_in}/subfam_enriched_UMR_[bchiklmstu]*.txt
+data_lmr=${dir_in}/subfam_enriched_LMR_[bchiklmstu]*.txt
+
+
+# OUTPUT
+dir_out=`pwd`
+
+out_prom=${dir_out}/subfam_enrich_ts_bp_filtered_active_promoter.txt
+out_weak=${dir_out}/subfam_enrich_ts_bp_filtered_weak_promoter.txt
+out_enhc=${dir_out}/subfam_enrich_ts_bp_filtered_active_enhancer.txt
+out_hete=${dir_out}/subfam_enrich_ts_bp_filtered_heterochromatin.txt
+
+out_prox=${dir_out}/subfam_enrich_ts_bp_filtered_proximal_ATAC.txt
+out_dist=${dir_out}/subfam_enrich_ts_bp_filtered_distal_ATAC.txt
+
+out_umr=${dir_out}/subfam_enrich_ts_bp_filtered_UMR.txt
+out_lmr=${dir_out}/subfam_enrich_ts_bp_filtered_LMR.txt
+
+
+# COMMANDS
+### Filtering
+### LOR > 2
+cat $data_prom |
+    grep -w -f <(cat $data_prom | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_prom
+
+cat $data_weak |
+    grep -w -f <(cat $data_weak | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_weak
+
+cat $data_enhc |
+    grep -w -f <(cat $data_enhc | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_enhc
+
+cat $data_hete |
+    grep -w -f <(cat $data_hete | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_hete
+
+cat $data_prox |
+    grep -w -f <(cat $data_prox | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_prox
+
+cat $data_dist |
+    grep -w -f <(cat $data_dist | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_dist
+
+cat $data_umr |
+    grep -w -f <(cat $data_umr | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_umr
+
+cat $data_lmr |
+    grep -w -f <(cat $data_lmr | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_lmr
+
+
+cat $data_tad |
+    grep -w -f <(cat $data_tad | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_tad
+
+cat $data_loop |
+    grep -w -f <(cat $data_loop | awk '$1!="NA" && $1>2' | cut -f2 | sort -u ) - > $out_loop
+
+
+
