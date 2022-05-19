99274e866643d4ea1637222e1471230eaf437c36
k326xh <nbsygr@gmail.com>
2022-05-18 21:50:26 -0500

    Update 3_proportion_individual_te_with_atac_ctcf.sh

--- 1_TE_annotations/3_proportion_individual_te_with_atac_ctcf.sh
+++ 1_TE_annotations/3_proportion_individual_te_with_atac_ctcf.sh
@@ -18,7 +18,7 @@ line=danRer10.LINE_frag.bed.gz
 sine=danRer10.SINE_frag.bed.gz
 rc=danRer10.RC_frag.bed.gz
 
-te_bed=$(ls ${dir_te}/danRer10.{TE,DNA,LTR,LINE,SINE,RC}_frag.bed.gz )
+te_bed=$(ls danRer10.{TE,DNA,LTR,LINE,SINE,RC}_frag.bed.gz )
 
 
 # INPUT DATA
@@ -31,7 +31,7 @@ loop=${dir_in}/loop_anchors/loop_anchors_${tissue}.atac_ctcf.bed.gz
 
 
 # OUTPUT
-dir_out=${workdir}/2_dynamic
+dir_out=2_dynamic
 out=${dir_out}/fraction_atac_ctcf_of_TE_in_${tissue}.txt
 
 
