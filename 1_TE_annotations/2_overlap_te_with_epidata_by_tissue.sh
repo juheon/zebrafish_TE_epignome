1e5a54a423fc0b88a7bfb89585d19eb8fd9596cf
k326xh <nbsygr@gmail.com>
2022-05-18 21:38:21 -0500

    Update 2_overlap_te_with_epidata_by_tissue.sh

--- 1_TE_annotations/2_overlap_te_with_epidata_by_tissue.sh
+++ 1_TE_annotations/2_overlap_te_with_epidata_by_tissue.sh
@@ -3,7 +3,6 @@
 
 #SBATCH --mem=2G
 #SBATCH --array=1-11
-##SBATCH --workdir=/scratch/twlab/hlee/zf_te
 #SBATCH --job-name=annot_feat_epi
 
 # SOFTWARE
@@ -11,13 +10,12 @@ module load bedtools/2.27.1
 ID=$SLURM_ARRAY_TASK_ID
 
 # GENOME FEATURES
-dir_te=/scratch/twlab/hlee/genomes/danRer10/rmsk
-te=${dir_te}/danRer10.TE.bed.gz
-dna=${dir_te}/danRer10.DNA.bed.gz
-ltr=${dir_te}/danRer10.LTR.bed.gz
-line=${dir_te}/danRer10.LINE.bed.gz
-sine=${dir_te}/danRer10.SINE.bed.gz
-rc=${dir_te}/danRer10.RC.bed.gz
+te=danRer10.TE.bed.gz
+dna=danRer10.DNA.bed.gz
+ltr=danRer10.LTR.bed.gz
+line=danRer10.LINE.bed.gz
+sine=danRer10.SINE.bed.gz
+rc=danRer10.RC.bed.gz
 
 te_bed=$(ls ${dir_te}/danRer10.{TE,DNA,LTR,LINE,SINE,RC}.bed.gz )
 
@@ -26,7 +24,7 @@ te_bed=$(ls ${dir_te}/danRer10.{TE,DNA,LTR,LINE,SINE,RC}.bed.gz )
 list=tissues-e.txt
 tissue=$( cat $list | sed "${ID}q;d" )
 
-dir_in=/scratch/twlab/hlee/zf_te/0_cres
+dir_in=0_cres
 prom=${dir_in}/activepromoter/${tissue}.active.promoter
 weak=${dir_in}/weakpromoter/${tissue}.weak.promoter
 enhc=${dir_in}/enhancer/${tissue}.active.enhancer
